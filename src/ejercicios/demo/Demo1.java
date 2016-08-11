14449d638cd55480b444c5740ad4db9a677e6e49
Dato Simó <asimo@ungs.edu.ar>
2016-08-11 15:45:54 -0300

    Incluir ejercicio Demo1 de ejemplo.

---
+++ src/ejercicios/demo/Demo1.java
@@ -0,0 +1,17 @@
+package ejercicios.demo;
+
+public class Demo1
+{
+	/**
+	 * Devuelve verdadero si el arreglo está ordenado.
+	 */
+	public static boolean estaOrdenado(int[] arreglo) {
+		boolean ret = true;
+
+		for (int i = 0; i < arreglo.length - 1; i++) {
+			ret = ret && (arreglo[i] < arreglo[i + 1]);
+		}
+
+		return ret;
+	}
+}
