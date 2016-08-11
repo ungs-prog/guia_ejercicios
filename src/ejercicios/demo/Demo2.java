163b432e006c5ca306fcc94f028dd15af9c5bb2b
Dato Simó <asimo@ungs.edu.ar>
2016-08-11 17:50:17 -0300

    Añadir ejercicios semana 1.

---
+++ src/ejercicios/demo/Demo2.java
@@ -0,0 +1,29 @@
+package ejercicios.demo;
+
+public class Demo2
+{
+	/**
+	 * Devuelve verdadero si todos los elementos están en el conjunto.
+	 */
+	public static boolean pertenecenTodos(int[] elems, int[] conjunto) {
+		return false;
+	}
+
+	/**
+	 * Devuelve el índice del pico en un arreglo unimodal.
+	 *
+	 * Nomenclatura: un arreglo unimodal de N elementos es estrictamente
+	 * creciente hasta una determinada posición P, y estrictamente
+	 * decreciente a partir de ella. Se cumple que 0 < P < N-1.
+	 *
+	 * Se garantiza que el arreglo pasado como parámetro tiene forma de
+	 * pico y, por tanto, al menos 3 elementos.
+	 */
+	public static int indicePico(int[] arreglo) {
+		// Ejemplos:
+		//     [2, 4, 6, 19, 15, 8, -2] → se devuelve 3
+		//     [10, 20, 30, 40, 50, 15] → se devuelve 4
+		//     [50, 100, 75]            → se devuelve 1
+		return -1;
+	}
+}
