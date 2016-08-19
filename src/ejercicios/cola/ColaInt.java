f5aa42036a630c96c5ad47e4db586967a7328971
Dato Simó <asimo@ungs.edu.ar>
2016-08-19 19:42:57 -0300

    Añádir ColaInt para la semana 2.

---
+++ src/ejercicios/cola/ColaInt.java
@@ -0,0 +1,26 @@
+package ejercicios.cola;
+
+/**
+ * Implementación de una cola ENLAZADA.
+ */
+public class ColaInt
+{
+	/*
+	 * TODOS los métodos con complejidad O(1).
+	 */
+
+	public void encolar(int elem) {
+	}
+
+	public boolean vacia() {
+		return true;
+	}
+
+	public Integer primero() {
+		return null;
+	}
+
+	public Integer desencolar() {
+		return null;
+	}
+}
