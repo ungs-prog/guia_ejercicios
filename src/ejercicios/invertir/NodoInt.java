3f7bb0e0d44d43c578aaef86f9a27121c3adfc34
Dato Simó <asimo@ungs.edu.ar>
2016-09-15 18:56:05 -0300

    Añadir ejercicios invertir pila/cola genéricas.

---
+++ src/ejercicios/invertir/NodoInt.java
@@ -0,0 +1,16 @@
+package ejercicios.invertir;
+
+class NodoInt
+{
+	int val;
+	NodoInt sig;
+
+	NodoInt(int elem) {
+		val = elem;
+	}
+
+	NodoInt(int elem, NodoInt nodo) {
+		val = elem;
+		sig = nodo;
+	}
+}
